913ff09f58851329b950828d0a159d738bb3f624
theshameem <theshameem.alam@gmail.com>
2021-09-17 12:34:47 +0600

    Filter out familiar skills

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from bs4 import BeautifulSoup
 import requests
 
-html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
+print("Put some skill that you are not familiar with")
+unfamiliar_skill = input('>')
+print(f"Filtering out {unfamiliar_skill}")
 
+html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
 soup = BeautifulSoup(html_text, 'lxml')
 jobs = soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
 
@@ -12,8 +15,10 @@ for job in jobs:
         company_name = job.find('h3', class_='joblist-comp-name').text.replace(' ', '')
         skills = job.find('span', class_='srp-skills').text.replace(' ', '')
         more_info = job.header.h2.a['href']
-        print(f"Company Name: {company_name.strip()}")
-        print(f"Required Skills: {skills.strip()}")
-        print(f"More Info: {more_info}")
+        
+        if unfamiliar_skill not in skills:
+            print(f"Company Name: {company_name.strip()}")
+            print(f"Required Skills: {skills.strip()}")
+            print(f"More Info: {more_info}")
 
-        print('')
+            print('')
